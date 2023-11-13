f4c2ca42769291f3949511f401f2ab1b96e71501
Austin Huynh <Austin.h54321@gmail.com>
2023-11-13 11:01:10 -0700

    Lab 4 Update
    Lab 4 complete

--- App.jsx
+++ App.jsx
@@ -17,10 +17,14 @@ function App() {
     'Study',
   ]);
 
+  const handleAddTask = task => {
+    setTasks([...tasks, task]);
+  };
+
   return (
     <SafeAreaView>
       <ToDoList tasks={tasks} />
-      <ToDoForm />
+      <ToDoForm onAddTask={handleAddTask} />
     </SafeAreaView>
   );
 }
